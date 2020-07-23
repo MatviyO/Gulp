e4ecb7c078d6d1a837c7c74604b8bff1f0d678c8
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-07-23 11:25:37 +0300

    st

--- simple2-gulp4--website/gulpfile.js
+++ simple2-gulp4--website/gulpfile.js
@@ -37,7 +37,12 @@ let {src, dest} = require('gulp'),
     clean_css = require('gulp-clean-css'),
     rename = require('gulp-rename'),
     uglify = require('gulp-uglify-es').default,
-    imagein = require('gulp-imagemin');
+    imagein = require('gulp-imagemin'),
+    webp = require('gulp-webp'),
+    webphtml = require('gulp-webp-html'),
+    webpcss = require('gulp-webpcss'),
+    ttf2woff = require('gulp-ttf2woff'),
+    ttf2woff2 = require('gulp-ttf2woff2')
 
 
 
@@ -54,6 +59,7 @@ function browserSync(params) {
 function html() {
     return src(path.src.html)
         .pipe(fileinclude())
+        .pipe(webphtml())
         .pipe(dest(path.build.html))
         .pipe(browsersync.stream())
 }
@@ -67,6 +73,7 @@ function css() {
             overrideBrowserslist: ["last 5 versions"],
             cascade: true
         }))
+        .pipe(webpcss())
         .pipe(dest(path.build.css))
         .pipe(clean_css())
         .pipe(rename({
@@ -75,8 +82,14 @@ function css() {
         .pipe(dest(path.build.css))
         .pipe(browsersync.stream())
 }
+
 function images() {
     return src(path.src.img)
+        .pipe(webp({
+            quality: 70
+        }))
+        .pipe(dest(path.build.img))
+        .pipe(src(path.src.img))
         .pipe(imagein({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
@@ -99,20 +112,30 @@ function js() {
         .pipe(dest(path.build.js))
         .pipe(browsersync.stream())
 }
+function fonts() {
+    src(path.src.fonts)
+        .pipe(ttf2woff())
+        .pipe(dest(path.build.fonts))
+    return src(path.src.fonts)
+        .pipe(ttf2woff2())
+        .pipe(dest(path.build.fonts))
+};
 
 function watchFiles(params) {
         gulp.watch([path.watch.html], html),
         gulp.watch([path.watch.css], css),
-            gulp.watch([path.watch.js], js)
+            gulp.watch([path.watch.js], js),
+            gulp.watch([path.watch.img], images)
 }
 function clean() {
     return del(path.clean)
 }
 
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images))
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts))
 let watch = gulp.parallel(build,watchFiles, browserSync);
 
+exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
 exports.css = css;
